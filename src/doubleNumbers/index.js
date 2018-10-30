69848124b0567401abac5dd7dfc769df5c0f3ef9
Vitaliy Pogoretskyy <vitpogoretsky@gmail.com>
2018-10-31 00:05:02 +0200

    The first and second assignments are ready

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,17 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const str = num.toString();
+  const strPart1 = str.slice(0, str.length / 2);
+  const strPart2 = str.slice(str.length / 2);
+		
+  return parseInt(strPart1) === parseInt(strPart2) ? num : num * 2;
 };
+
+
+
+
+
+
+
+
+
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
+const getWordWeight = word => {
+  return word.split('').reduce((letterAcc, letterElem) => {
+    return parseInt(letterAcc) + parseInt(letterElem);
+  });  
+};
+
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr.sort((firstWord, secondWord) => {
+    return getWordWeight(firstWord) > getWordWeight(secondWord);
+  });
 };
